0340b707ff150257313c127c0526f55adf2f1233
github username <github email address>
2019-04-28 21:48:10 +0100

    all of it except Artist/most_prolific

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,44 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings #works
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries #works
+    paintings.map {|paintings| paintings.gallery}
+  end
+
+  def cities #works
+    galleries.map {|galleries| galleries.city}
+  end
+
+  def self.total_experience #works
+    total_years = all.map {|artist| artist.years_experience}
+    total_years.sum
+  end
+
+  def self.most_prolific
+
+    # can I call the paintings_hash?
+  end
+    #artist with the highest number of paintings / years of experience. I have number of paintings per artist and number of experience per artist.
+
+
+  def create_painting(title, price, gallery) #works
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,35 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price #works
+    total = all.map {|price| price.price} #if I'm already in a class method, do I do all map or Paintings.all.map?
+    total.sum
+  end
+
+  def self.paintings_hash
+    artists_paintings = {}
+    Painting.all.each do |painting|
+      if artists_paintings[painting]
+        artists_paintings[painting] += 1
+      else
+        artists_paintings[painting] =1
+      end
+    end
+    artists_paintings
+  end
 end
