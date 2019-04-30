9aa53014b515bb55444960136b24443427a1ac27
lavikoder <kevin.kamanda@flatironschool.com>
2019-04-30 20:33:11 +0100

    Gallery

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,60 @@
+# * `Artist.most_prolific`
+#   * Returns an `instance` of the artist with the highest amount of paintings per year of experience.
+
+
+
+
 class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def painting
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def gallery
+    painting.map { |painting| painting.gallery }
+  end
+
+  def cities
+    painting.map {|painting| painting.gallery.city}
+  end
+
+  def self.total_experience
+    @@all.map{|artist| artist.years_experience}.sum
+  end
+
+  def painting_count
+    painting.length
+  end
+
+  def self.years_experience
+    Artist.select {|artist| artist.years_experience}
+  end
+
+  def paint_per_year
+    painting_count.to_f/self.years_experience
+  end
+
+  def self.most_prolific
+    @@all.max_by{|artist| artist.paint_per_year}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,32 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def painting
+    Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artist
+    painting.map {|painting| painting.artist}
+  end
+
+  def artist_name
+    artist.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    painting.max_by {|painting| painting.price}
   end
 
 end
